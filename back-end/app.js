14a1246765673d02eb6b9b34ad3796eceecc23f6
Henrique Herbs Rosa <henriquehhr@gmail.com>
2022-04-25 12:03:41 -0300

    feature: added sign-up post function

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,17 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
 
 const app = express();
 const PORT = 5000;
 app.use(cors());
+app.use(json());
+
+const users = [];
+const tweets = [];
 
 app.listen(PORT, () => console.log(`Server live at http://localhost:${PORT}/`));
+
+app.post("/sign-up", (req, res) => {
+    users.push(req.body);
+    res.send("OK");
+});
